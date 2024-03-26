ec9a3e08448815ec7f05ebfc46a0abefb0c3906d
ygshen200@163.com <ygshen200@163.com>
2024-03-26 13:26:32 +0800

    trigger java exe

---
+++ main/src/test/java/com/oreily/OreilyResourceTest.java
@@ -0,0 +1,11 @@
+package com.oreily;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OreilyResourceTest {
+    @Test
+    public void doTest() {
+        Assertions.assertTrue(true);
+    }
+}
